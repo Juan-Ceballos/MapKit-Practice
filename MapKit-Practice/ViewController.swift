e364c88a0f16b5f0a2e9ab56c7765d7830dbfb85
Juan C <juanceballos@pursuit.org>
2022-03-18 21:26:47 -0400

    mapkit practice setup

---
+++ MapKit-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapKit-Practice
+//
+//  Created by Juan Ceballos on 3/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
