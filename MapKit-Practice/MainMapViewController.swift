28cc2b9a083d63f2166215f1d0ebd7da110cbee0
Juan C <juanceballos@pursuit.org>
2022-03-25 17:59:12 -0400

    when didUpdate userLocation called, uses userLocation to create region and checks all locations to see if within region if is makes annotation and adds to map

--- MapKit-Practice/MainMapViewController.swift
+++ MapKit-Practice/MainMapViewController.swift
@@ -22,30 +22,35 @@ class MainMapViewController: UIViewController {
         //convertPlacenameToCoordinate()
         mainView.mKMapView.showsUserLocation = true
         mainView.mKMapView.delegate = self
-        loadMapView()
+        //loadMapView()
     }
     
-    private func makeAnnotations() -> [MKPointAnnotation] {
+    private func makeAnnotations(userCoord: CLLocationCoordinate2D, id: String) -> [MKPointAnnotation] {
         var annotations = [MKPointAnnotation]()
+        let currentRegion = CLCircularRegion(center: userCoord, radius: 400, identifier: id)
         for location in Location.getLocations() {
             // if location within region do this, maybe get the location of all users
             // or research device tracks registered devices in area
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = location.coordinate
-            annotation.title = location.title
-            annotations.append(annotation)
+            if currentRegion.contains(location.coordinate) {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = location.coordinate
+                annotation.title = location.title
+                annotations.append(annotation)
+            }
         }
         
         return annotations
     }
     
-    private func loadMapView() {
-        let annotations = makeAnnotations()
-        // since region is set will only see nearby annotations unless zoomout
-        mainView.mKMapView.addAnnotations(annotations)
-        //mainView.mKMapView.showAnnotations(annotations, animated: false)
-        //mainView.mKMapView.annotations(in: _)
-    }
+//    private func loadMapView() {
+//        let annotations = makeAnnotations()
+//        print("user coord from load map")
+//        print(mainView.mKMapView.userLocation.coordinate)
+//        // since region is set will only see nearby annotations unless zoomout
+//        mainView.mKMapView.addAnnotations(annotations)
+//        //mainView.mKMapView.showAnnotations(annotations, animated: false)
+//        //mainView.mKMapView.annotations(in: _)
+//    }
 
     public func convertCoordinateToPlacemark() {
         if let location = Location.getLocations().first {
@@ -84,9 +89,13 @@ extension MainMapViewController: MKMapViewDelegate {
         // stuff here
     }
     
+    // set region for zoom in
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
         let userCoord = mapView.userLocation.coordinate
         print("this is user loc: \(userCoord)")
+        let annotations = makeAnnotations(userCoord: userCoord, id: "idLoc")
+        mapView.addAnnotations(annotations)
+        //mapView.showAnnotations(annotations, animated: false)
         let region = MKCoordinateRegion(center: userCoord, latitudinalMeters: 400, longitudinalMeters: 400)
         mainView.mKMapView.setRegion(region, animated: true)
     }
